3199108c6473222949846c139bd5746d66a2b073
Ademir Espinoza <ademirjesusespinozalozano@gmail.com>
2023-04-18 17:28:06 -0500

    Toolbarr (ok) Tittle Toolbar (ok)

--- app/src/main/java/com/example/apptest/MainActivity.kt
+++ app/src/main/java/com/example/apptest/MainActivity.kt
@@ -1,30 +1,24 @@
 package com.example.apptest
-
-import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
-import com.google.android.material.navigation.NavigationView
-
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        toolbar.setOverflowIcon(ContextCompat.getDrawable(this, R.drawable.baseline_menu_24))
+
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.submenus, menu)
+        menuInflater.inflate(R.menu.activity_main_drawer, menu)
         return true
     }
 
@@ -35,95 +29,3 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 }
-
-/*
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
-    private lateinit var drawer: DrawerLayout
-    private lateinit var toogle: ActionBarDrawerToggle
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        */
-/*val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)*//*
-
-        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
-        setSupportActionBar(toolbar)
-
-        drawer = findViewById(R.id.drawer_layout)
-
-        toogle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer.addDrawerListener(toogle)
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeButtonEnabled(true)
-
-        val navigationView: NavigationView = findViewById(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener(this)
-
-*/
-/*
-        supportFragmentManager.commit {
-            replace<LogIpConfigFragment>(R.id.frameContainer)
-            setReorderingAllowed(true)
-            addToBackStack("replacement")
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.activity_main_drawer, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.settings -> {
-                // Acción a realizar cuando se selecciona el subítem 1
-                return true
-            }
-            R.id.hardware -> {
-                // Acción a realizar cuando se selecciona el subítem 2
-                return true
-            }
-            R.id.reports -> {
-                // Acción a realizar cuando se selecciona el ítem 2
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-}*//*
-
-
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_item_logout -> Toast.makeText(this, "Desconectar", Toast.LENGTH_SHORT).show()
-        }
-
-        drawer.closeDrawer(GravityCompat.START)
-        return true
-    }
-
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        toogle.syncState()
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        toogle.onConfigurationChanged(newConfig)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toogle.onOptionsItemSelected(item)) {
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-}*/
