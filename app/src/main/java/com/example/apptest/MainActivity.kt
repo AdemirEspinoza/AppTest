4e24061da16cd2d2e061607cc2cb1cca56f8a236
Ademir Espinoza <ademirjesusespinozalozano@gmail.com>
2023-04-18 17:28:06 -0500

    V2

--- app/src/main/java/com/example/apptest/MainActivity.kt
+++ app/src/main/java/com/example/apptest/MainActivity.kt
@@ -10,12 +10,12 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)*/
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
         setSupportActionBar(toolbar)
 
