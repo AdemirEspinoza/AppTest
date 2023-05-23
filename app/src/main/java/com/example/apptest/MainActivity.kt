7b61ea2b7fed96071f494938a5a28c627d2e3f4e
Ademir Espinoza <ademirjesusespinozalozano@gmail.com>
2023-05-23 17:19:40 -0500

    Toolbarr (ok) Tittle Toolbar (ok) Nuevo Toolbar (ok) Home v2 (ok) //Falta arreglar el responsive de las vistas

--- app/src/main/java/com/example/apptest/MainActivity.kt
+++ app/src/main/java/com/example/apptest/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_search_stock_product)
 
 
     }
