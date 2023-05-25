8b33e661cdefcaa3e916c8155e9694a336b5442f
Ademir Espinoza <ademirjesusespinozalozano@gmail.com>
2023-05-25 17:25:48 -0500

    Comenzar a crear la lógica de navegación entre botones y vistas

--- app/src/main/java/com/example/apptest/MainActivity.kt
+++ app/src/main/java/com/example/apptest/MainActivity.kt
@@ -7,24 +7,13 @@ import android.view.Window
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
     }
-/*    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.activity_main_drawer, menu)
-        return true
-    }*/
-
-/*    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.edit_action) {
-            Toast.makeText(this, "Edición", Toast.LENGTH_SHORT).show()
-        }
-        return true
-    }*/
 }
 
