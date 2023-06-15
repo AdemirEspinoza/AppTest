87fa27499e25390823d3773b9049c86b83d6d1b9
Ademir Espinoza <ademirjesusespinozalozano@gmail.com>
2023-06-15 13:30:11 -0500

    Actualización 08-06-2023 (Turno Tarde) - Conectar el btn Seach de Home Fragment

--- app/src/main/java/com/example/apptest/HomeFragment.kt
+++ app/src/main/java/com/example/apptest/HomeFragment.kt
@@ -9,13 +9,22 @@ import android.widget.Button
 import android.widget.GridLayout
 import android.widget.Spinner
 import androidx.cardview.widget.CardView
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import com.example.apptest.databinding.FragmentHomeBinding
+import com.example.apptest.databinding.FragmentLogPinBinding
+import com.example.apptest.util.viewBinding
 
+/**
+ * A simple [Fragment] subclass.
+ */
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private val binding by viewBinding (FragmentHomeBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        }
-
+        binding.btn_search.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_searchStockProductFragment)}
+    }
 }
--- app/src/main/java/com/example/apptest/fragments/LogPinFragment.kt
+++ app/src/main/java/com/example/apptest/fragments/LogPinFragment.kt
@@ -1,25 +1,27 @@
 package com.example.apptest.fragments
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+
 import com.example.apptest.R
-import com.example.apptest.databinding.FragmentLogConfigIpBinding
+import com.example.apptest.databinding.FragmentLogClientLicBinding
+import com.example.apptest.databinding.FragmentLogPinBinding
 import com.example.apptest.util.viewBinding
 
 /**
  * A simple [Fragment] subclass.
  */
-class LogIpConfigFragment : Fragment(R.layout.fragment_log_config_ip) {
+class LogCPinFragment : Fragment(R.layout.fragment_log_pin) {
 
-    private val binding by viewBinding(FragmentLogConfigIpBinding::bind)
+    private val binding by viewBinding (FragmentLogPinBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnLogIpConfig.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_logIpConfigFragment_to_logClientLicFragment)}
-
+        binding.btnLogPin.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.action_logPinFragment_to_navigation_search)}
     }
 }
