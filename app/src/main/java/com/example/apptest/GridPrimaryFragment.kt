822f0e69ca2e5894464ce5c327c96e7476df0936
Ademir Espinoza <ademirjesusespinozalozano@gmail.com>
2023-06-22 11:50:05 -0500

    Actualización 22-06-2023 (Turno Mañana) - Conectar Fragmen "logPinFragment" "homeFragment" y dar onClic a cardview "Busqueda"

---
+++ app/src/main/java/com/example/apptest/GridPrimaryFragment.kt
@@ -0,0 +1,16 @@
+package com.example.apptest
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GridPrimaryFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_grid_primary, container, false)
+    }
+}
+
--- app/src/main/java/com/example/apptest/HomeFragment.kt
+++ app/src/main/java/com/example/apptest/HomeFragment.kt
@@ -1,17 +1,28 @@
-package com.example.apptest
-
+package com.example.apptest.fragments
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.GridLayout
-import android.widget.Spinner
-import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
+import com.example.apptest.R
 import com.example.apptest.databinding.FragmentHomeBinding
-import com.example.apptest.databinding.FragmentLogPinBinding
+import com.example.apptest.databinding.FragmentLogConfigIpBinding
+import com.example.apptest.databinding.ItemCardviewOtherSearchBinding
 import com.example.apptest.util.viewBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnBusqueda.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_searchStockProductFragment)}
+
+    }
+}
 
